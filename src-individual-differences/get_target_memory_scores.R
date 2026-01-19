c8ac4123acab2bee64aef9bade3b14da7a671fcf
Emily Chung <echung@ad.life.unsw.edu.au>
2026-01-19 14:21:13 +1100

    getting target memory data

---
+++ src-individual-differences/get_target_memory_scores.R
@@ -0,0 +1,13 @@
+# E. Chung, 2026
+# getting scores for memory of target locations at the end of the doors task.
+
+library(tidyverse)
+
+memory_data <- read.csv("C:/Users/echung/UNSW/Learning and Attention Group - Emily PhD project - exp1_data/individual_differences/target_locs_data.csv")
+
+memory_data <- memory_data %>% 
+  group_by(sub) %>% 
+  mutate(mean_locs_correct = (ncorrectA + ncorrectB)/2) 
+  
+
+write.csv(memory_data, file = "res/target_memory_data.csv", row.names = FALSE)
