b53c18f3c10fb060ccdd4a5a6fc8f40b31a37db4
Emily Chung <echung@ad.life.unsw.edu.au>
2026-01-19 14:21:29 +1100

    compiling individual differences data into one dataframe

---
+++ src-individual-differences/get_individual_differences.R
@@ -0,0 +1,29 @@
+#E. Chung, 2026
+# this script is used to bind all the individual differences data into one csv file.
+
+library(tidyverse)
+library(dplyr)
+
+# reading all the data files
+corsi <- read.csv('res/corsi_scores.csv')
+digit_span <- read.csv('res/digit_span_scores.csv')
+target_memory <- read.csv('res/target_memory_data.csv')
+self_report <- read.csv('res/rule_learning_data.csv')
+
+# merging data
+merged_data <- corsi %>%
+  inner_join(digit_span, by = "sub") %>%
+  inner_join(target_memory, by = "sub") %>% 
+  inner_join(self_report, by = "sub")
+
+
+# adding a columns with group membership
+group_list <- rep(c("Stable", "Variable"), times = nrow(merged_data)/2)
+merged_data <- merged_data %>% 
+  mutate(train_type = group_list)
+
+merged_data <- merged_data %>%
+  relocate(sub, train_type, task_config, .before = everything())
+
+
+write.csv(merged_data, file = "res/all_individual_differences.csv", row.names = FALSE)
